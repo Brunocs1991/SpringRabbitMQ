b97330fb4a8fd565aaa6fe5168adbeee683ef8a1
Bruno <brunocosta.1991@hotmail.com>
2022-05-18 20:58:03 -0300

    Curso Rabbit

---
+++ spring-producer/src/main/java/br/com/github/springproducer/SpringProducerApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.springproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProducerApplication.class, args);
+    }
+
+}
